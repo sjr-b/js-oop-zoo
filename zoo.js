b798a11a6b806415c674d1d3933998f7365c071d
Sophia Rodriguez-Bell <sophiarodriguezbell@students.berkeley.net>
2018-12-12 14:26:12 -0800

    keep goin'

--- zoo.js
+++ zoo.js
@@ -15,3 +15,22 @@ function eat(name, food){
     console.log(name + " eats " + food);
     (favoriteFood == food) ? console.log("YUM!!! " + name + " wants more " + food) : sleep(name);
 }
+
+class Tiger {
+
+    constructor(name) {
+        this.name = name;
+        this.favoriteFood = "bacon";
+    }
+
+    sleep() {
+        // complete your sleep function here
+
+    }
+
+    eat(food) {
+        // complete your eat function here!
+
+    }
+
+}
